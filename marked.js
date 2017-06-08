c42c3fad7d22b4b24bbeed55984b53995809b566
高淼淼 <gaomiaomiao@fengweiniurougan.local>
2017-06-08 11:10:22 +0800

    change: 同步写文件接口没有callback

--- marked.js
+++ marked.js
@@ -106,35 +106,26 @@ function generateNavHtml(domTree, $nav) {
 
     }
 }
-function generateFile(input, outHtml, outNav){
+function generateFile(input, outHtml, outNav) {
     // 读取markdowm文件
     markdownString = fs.readFileSync(path.join(cwd, input), 'utf-8');
     // 生成html字符串
     htmlString = marked(markdownString, { renderer: renderer });
     // 写content.html文件
-    fs.writeFileSync(path.join(cwd, outHtml), htmlString, 'utf-8', (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log('The file has been saved!');
-    });
+    fs.writeFileSync(path.join(cwd, outHtml), htmlString, 'utf-8');
 
     let $ = cheerio.load(htmlString);
     let domTree = generateNavObj($, 2, 3);
     let $nav = cheerio.load('<ul class="m-doc-nav"></ul>')('ul.m-doc-nav');
     generateNavHtml(domTree, $nav);
     // 写nav.html文件
-    fs.writeFileSync(path.join(cwd, outNav), $nav.toString(), 'utf-8', (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log('The file has been saved!');
-    });
+    fs.writeFileSync(path.join(cwd, outNav), $nav.toString(), 'utf-8');
 }
-function generateFiles(config){
-    config = config;    
+
+function generateFiles(config) {
+    config = config;
     let fileConfig = config.inputAndOutputInfo;
-    for (var i = 0; i < fileConfig.length; i++){
+    for (var i = 0; i < fileConfig.length; i++) {
         generateFile(fileConfig[i].inputFileName, fileConfig[i].outputContentName, fileConfig[i].outputNavName);
     }
 }
